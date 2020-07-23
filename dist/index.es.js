e85d9dace9f017af88b1aba19aef97d4dcaaa9f5
inotom <wdf7322@yahoo.co.jp>
2020-07-23 16:30:04 +0900

    Modify tsconfig target

--- dist/index.es.js
+++ dist/index.es.js
@@ -17,33 +17,33 @@ var src = createCommonjsModule(function (module, exports) {
 /** @prettier */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.toKatakana = exports.toHiragana = exports.HiraganaToKatakana = exports.KatakanaToHiragana = exports.isHiragana = exports.isKatakana = void 0;
-exports.isKatakana = (c) => {
-    const code = c.charCodeAt(0);
+exports.isKatakana = function (c) {
+    var code = c.charCodeAt(0);
     // ァ:12449
     // ヶ:12534
     return code >= 12449 && code <= 12534;
 };
-exports.isHiragana = (c) => {
-    const code = c.charCodeAt(0);
+exports.isHiragana = function (c) {
+    var code = c.charCodeAt(0);
     // ぁ:12353
     // ゖ:12438
     return code >= 12353 && code <= 12438;
 };
-exports.KatakanaToHiragana = (c) => {
+exports.KatakanaToHiragana = function (c) {
     if (!exports.isKatakana(c)) {
         return c;
     }
     return String.fromCharCode(c.charCodeAt(0) - 96);
 };
-exports.HiraganaToKatakana = (c) => {
+exports.HiraganaToKatakana = function (c) {
     if (!exports.isHiragana(c)) {
         return c;
     }
     return String.fromCharCode(c.charCodeAt(0) + 96);
 };
-const convertTo = (str, fn) => Array.prototype.map.call(str, fn).join('');
-exports.toHiragana = (str) => convertTo(str, exports.KatakanaToHiragana);
-exports.toKatakana = (str) => convertTo(str, exports.HiraganaToKatakana);
+var convertTo = function (str, fn) { return Array.prototype.map.call(str, fn).join(''); };
+exports.toHiragana = function (str) { return convertTo(str, exports.KatakanaToHiragana); };
+exports.toKatakana = function (str) { return convertTo(str, exports.HiraganaToKatakana); };
 
 });
 
